344d8fac267f6f642afab2aafc2359ef86ac6883
Gavin M. Roy <gavinr@aweber.com>
2015-07-20 21:48:16 -0400

    Add setup files

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+import sys
+
+tests_require = ['nose', 'mock']
+if sys.version_info < (2, 7, 0):
+    tests_require.append('unittest2')
+
+desc = 'An opinionated fleet API client for Python'
+
+classifiers = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: BSD License',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 2.6',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 3.2',
+               'Programming Language :: Python :: 3.3',
+               'Programming Language :: Python :: 3.4',
+               'Programming Language :: Python :: Implementation :: CPython',
+               'Programming Language :: Python :: Implementation :: PyPy',
+               'Topic :: Communications',
+               'Topic :: Internet',
+               'Topic :: Software Development :: Libraries']
+
+setuptools.setup(name='fleetpy',
+                 version='0.1.0',
+                 description=desc,
+                 long_description=open('README.rst').read(),
+                 author='Gavin M. Roy',
+                 author_email='gavinmroy@gmail.com',
+                 url='http://fleetpy.readthedocs.org',
+                 packages=['fleetpy'],
+                 package_data={'': ['LICENSE', 'README.rst']},
+                 include_package_data=True,
+                 install_requires=['requests'],
+                 tests_require=tests_require,
+                 license='BSD',
+                 classifiers=classifiers,
+                 zip_safe=True)
